8e74e54d9870809160af52815ed4573f91450563
teufel46 <9104785163@mail.ru>
2022-07-13 17:32:36 +0300

    add authentication

---
+++ app/src/main/java/ru/netology/nmedia/activity/LoginFragment.kt
@@ -0,0 +1,57 @@
+package ru.netology.nmedia.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.nmedia.api.PostsApi
+import ru.netology.nmedia.auth.AppAuth
+import ru.netology.nmedia.databinding.FragmentLoginBinding
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class LoginFragment : Fragment() {
+
+    private var fragmentBinding : FragmentLoginBinding? = null
+
+    private val viewModel: PostViewModel by viewModels(
+        ownerProducer = ::requireParentFragment,
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentLoginBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        fragmentBinding = binding
+
+        binding.username.requestFocus()
+
+        binding.login.setOnClickListener {
+            viewModel.updateUser(
+          //      "netology",
+                binding.username.text.toString(),
+          //      "secret",
+                binding.password.text.toString()
+                 )
+            findNavController().navigateUp()
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        fragmentBinding = null
+        super.onDestroyView()
+    }
+}
